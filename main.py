2a88ca8d97fe39b680886eccfdccc08e6e513159
Mary <68235230+maryjng@users.noreply.github.com>
2020-09-25 13:29:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,20 @@ def sevensegment():
     else: print("invalid entry")
 
 sevensegment()
+
+
+##### starting hexadecimal A b C d E F
+
+hexDict = {"A" : (" # ", "# #", "###", "# #", "# #"), "b" : ("#  ", "#  ", "###", "# #", "###"), "C" : ("###", "#  ", "#  ", "#  ", "###"), "d" : ("  #", "  #", "###", "# #", "###"), "E" : ("###", "#  ", "###", "#  ", "###"), "F" : ("###", "#  ", "###", "#  ", "#  ")}
+           
+def hexadecimal():
+    segment = input()
+    for ch in segment:
+        if ch not in hexDict:
+           print("invalid entry")
+           return
+        else: templist.append(hexDict[ch])
+        for n in range(5):
+            hexadecimal = [tup[n] for tup in templist]
+            print(" ".join(hexadecimal))
+            
